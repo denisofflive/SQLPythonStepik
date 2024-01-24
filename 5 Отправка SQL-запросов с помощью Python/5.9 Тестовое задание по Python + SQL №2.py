a80ea6c16350c8dc19d63e57e18c4ab7647de559
denis <denisofflive@mail.ru>
2024-01-24 14:46:35 +0300

    fix 5.9 Тестовое задание по Python + SQL №2

--- 5 Отправка SQL-запросов с помощью Python/5.9 Тестовое задание по Python + SQL №2.py
+++ 5 Отправка SQL-запросов с помощью Python/5.9 Тестовое задание по Python + SQL №2.py
@@ -21,9 +21,9 @@ cur.execute("""INSERT INTO users_balance(Balance_RUB, Balance_USD, Balance_EUR)
 """, user)
 db.commit()
 
-rub = '1'
-usd = '2'
-eur = '3'
+RUB = '1'
+USD = '2'
+EUR = '3'
 
 '''Приветствие'''
 while True:
@@ -38,7 +38,7 @@ while True:
                    '\n1 - RUB'
                    '\n2 - USD'
                    '\n3 - EUR\n')
-    print('Вы выбрали - ' + str(answer))
+    print(f'Вы выбрали - {answer}')
 
     if answer == '1' or answer == '2' or answer == '3':
 
@@ -53,20 +53,20 @@ while True:
                              '\n1 - RUB'
                              '\n2 - USD'
                              '\n3 - EUR\n')
-            print('Вы выбрали - ' + str(currency))
+            print(f'Вы выбрали -  {currency}')
             if currency == '1' or currency == '2' or currency == '3':
 
                 """ОШИБКА - Выбор одной валюты"""
 
                 if (answer == '1' and currency == '1') or (answer == '2' and currency == '2') or (
                         answer == '3' and currency == '3'):
-                    print('Вы выбрали неподходяющую валюту!')
+                    print('Вы выбрали неподходящую валюту!')
                     break
 
                 """Подсчёт количества нужной валюты"""
 
                 # RUB
-                if answer == rub:
+                if answer == RUB:
 
                     request_rub = cur.execute("""SELECT Balance_RUB FROM users_balance;""")
                     date_rub = cur.fetchone()
@@ -76,56 +76,60 @@ while True:
                         break
 
                     else:
-                        """Операция по вычеслению и изменению баланса пользователя"""
+                        """Операция по вычислению и изменению баланса пользователя"""
 
                         # RUB -> USD
-                        if currency == usd:
+                        if currency == USD:
 
-                            exch_rub = float(date_rub[0]) + float(amount)
+                            exchange_rub = float(date_rub[0]) + float(amount)
 
                             # Изменяем количество средств RUB
                             update_rub = cur.execute(
-                                f"""UPDATE users_balance SET Balance_RUB = '{exch_rub}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_RUB = '{exchange_rub}' WHERE UserID = '1';""")
                             db.commit()
 
                             request_usd = cur.execute("""SELECT Balance_USD FROM users_balance;""")
                             date_usd = cur.fetchone()
 
                             amount_usd = float(amount) / float(70)
-                            exch_usd = float(date_usd[0]) - float(amount_usd)
+                            exchange_usd = float(date_usd[0]) - float(amount_usd)
 
                             # Изменяем количество средств USD
                             update_usd = cur.execute(
-                                f"""UPDATE users_balance SET Balance_USD = '{exch_usd}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_USD = '{exchange_usd}' WHERE UserID = '1';""")
                             db.commit()
 
-                            print(f'Обмен валюты совершён! Баланс RUB - {int(exch_rub)}, баланс USD - {int(exch_usd)}')
+                            print(
+                                f'Обмен валюты успешно завершен! Баланс RUB - {int(exchange_rub)},'
+                                f' баланс USD - {int(exchange_usd)}')
                             break
                         # RUB -> EUR
-                        elif currency == eur:
+                        elif currency == EUR:
 
-                            exch_rub = float(date_rub[0]) + float(amount)
+                            exchange_rub = float(date_rub[0]) + float(amount)
 
                             # Изменяем количество средств RUB
                             update_rub = cur.execute(
-                                f"""UPDATE users_balance SET Balance_RUB = '{exch_rub}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_RUB = '{exchange_rub}' WHERE UserID = '1';""")
                             db.commit()
 
                             request_eur = cur.execute("""SELECT Balance_EUR FROM users_balance;""")
                             date_eur = cur.fetchone()
 
                             amount_eur = float(amount) / float(80)
-                            exch_eur = float(date_eur[0]) - float(amount_eur)
+                            exchange_eur = float(date_eur[0]) - float(amount_eur)
 
                             # Изменяем количество средств EUR
                             update_eur = cur.execute(
-                                f"""UPDATE users_balance SET Balance_EUR = '{exch_eur}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_EUR = '{exchange_eur}' WHERE UserID = '1';""")
                             db.commit()
 
-                            print(f'Обмен валюты совершён! Баланс RUB - {int(exch_rub)}, баланс EUR - {int(exch_eur)}')
+                            print(
+                                f'Обмен валюты успешно завершен! Баланс RUB - {int(exchange_rub)},'
+                                f' баланс EUR - {int(exchange_eur)}')
                             break
                 # USD
-                if answer == usd:
+                if answer == USD:
 
                     request_usd = cur.execute("""SELECT Balance_USD FROM users_balance;""")
                     date_usd = cur.fetchone()
@@ -135,55 +139,59 @@ while True:
                         break
                     else:
                         # USD -> RUB
-                        if currency == rub:
+                        if currency == RUB:
 
-                            exch_usd = float(date_usd[0]) + float(amount)
+                            exchange_usd = float(date_usd[0]) + float(amount)
 
                             # Изменяем количество средств USD
                             update_usd = cur.execute(
-                                f"""UPDATE users_balance SET Balance_USD = '{exch_usd}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_USD = '{exchange_usd}' WHERE UserID = '1';""")
                             db.commit()
 
                             request_rub = cur.execute("""SELECT Balance_RUB FROM users_balance;""")
                             date_rub = cur.fetchone()
 
                             amount_rub = float(amount) * float(70)
-                            exch_rub = float(date_rub[0]) - float(amount_rub)
+                            exchange_rub = float(date_rub[0]) - float(amount_rub)
 
                             # Изменяем количество средств RUB
                             update_rub = cur.execute(
-                                f"""UPDATE users_balance SET Balance_RUB = '{exch_rub}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_RUB = '{exchange_rub}' WHERE UserID = '1';""")
                             db.commit()
 
-                            print(f'Обмен валюты совершён! Баланс USD - {int(exch_usd)}, баланс RUB - {int(exch_rub)}')
+                            print(
+                                f'Обмен валюты успешно завершен! Баланс USD - {int(exchange_usd)},'
+                                f' баланс RUB - {int(exchange_rub)}')
                             break
 
                         # USD -> EUR
-                        elif currency == eur:
+                        elif currency == EUR:
 
-                            exch_usd = float(date_usd[0]) + float(amount)
+                            exchange_usd = float(date_usd[0]) + float(amount)
 
                             # Изменяем количество средств USD
                             update_usd = cur.execute(
-                                f"""UPDATE users_balance SET Balance_USD = '{exch_usd}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_USD = '{exchange_usd}' WHERE UserID = '1';""")
                             db.commit()
 
                             request_eur = cur.execute("""SELECT Balance_EUR FROM users_balance;""")
                             date_eur = cur.fetchone()
 
                             amount_eur = float(amount) / float(0.87)
-                            exch_eur = float(date_eur[0]) - float(amount_eur)
+                            exchange_eur = float(date_eur[0]) - float(amount_eur)
 
                             # Изменяем количество средств EUR
                             update_eur = cur.execute(
-                                f"""UPDATE users_balance SET Balance_EUR = '{exch_eur}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_EUR = '{exchange_eur}' WHERE UserID = '1';""")
                             db.commit()
 
-                            print(f'Обмен валюты совершён! Баланс USD - {int(exch_usd)}, баланс EUR - {int(exch_eur)}')
+                            print(
+                                f'Обмен валюты успешно завершен! Баланс USD - {int(exchange_usd)},'
+                                f' баланс EUR - {int(exchange_eur)}')
                             break
 
                 # EUR
-                if answer == eur:
+                if answer == EUR:
 
                     request_eur = cur.execute("""SELECT Balance_EUR FROM users_balance;""")
                     date_eur = cur.fetchone()
@@ -193,50 +201,54 @@ while True:
                         break
                     else:
                         # EUR -> RUB
-                        if currency == rub:
+                        if currency == RUB:
 
-                            exch_eur = float(date_eur[0]) + float(amount)
+                            exchange_eur = float(date_eur[0]) + float(amount)
 
                             # Изменяем количество средств EUR
                             update_eur = cur.execute(
-                                f"""UPDATE users_balance SET Balance_EUR = '{exch_eur}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_EUR = '{exchange_eur}' WHERE UserID = '1';""")
                             db.commit()
 
                             request_rub = cur.execute("""SELECT Balance_RUB FROM users_balance;""")
                             date_rub = cur.fetchone()
 
                             amount_rub = float(amount) * float(80)
-                            exch_rub = float(date_rub[0]) - float(amount_rub)
+                            exchange_rub = float(date_rub[0]) - float(amount_rub)
 
                             # Изменяем количество средств RUB
                             update_rub = cur.execute(
-                                f"""UPDATE users_balance SET Balance_RUB = '{exch_rub}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_RUB = '{exchange_rub}' WHERE UserID = '1';""")
                             db.commit()
 
-                            print(f'Обмен валюты совершён! Баланс EUR - {int(exch_eur)}, баланс RUB - {int(exch_rub)}')
+                            print(
+                                f'Обмен валюты успешно завершен! Баланс EUR - {int(exchange_eur)},'
+                                f' баланс RUB - {int(exchange_rub)}')
                             break
                         # EUR -> USD
-                        elif currency == usd:
+                        elif currency == USD:
 
-                            exch_eur = float(date_eur[0]) + float(amount)
+                            exchange_eur = float(date_eur[0]) + float(amount)
 
                             # Изменяем количество средств EUR
                             update_eur = cur.execute(
-                                f"""UPDATE users_balance SET Balance_EUR = '{exch_eur}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_EUR = '{exchange_eur}' WHERE UserID = '1';""")
                             db.commit()
 
                             request_usd = cur.execute("""SELECT Balance_USD FROM users_balance;""")
                             date_usd = cur.fetchone()
 
                             amount_usd = float(amount) * float(1.15)
-                            exch_usd = float(date_eur[0]) - amount_usd
+                            exchange_usd = float(date_eur[0]) - amount_usd
 
                             # Изменяем количество средств USD
                             update_usd = cur.execute(
-                                f"""UPDATE users_balance SET Balance_USD = '{exch_usd}' WHERE UserID = '1';""")
+                                f"""UPDATE users_balance SET Balance_USD = '{exchange_usd}' WHERE UserID = '1';""")
                             db.commit()
 
-                            print(f'Обмен валюты совершён! Баланс EUR - {int(exch_eur)}, баланс USD - {int(exch_usd)}')
+                            print(
+                                f'Обмен валюты успешно завершен! Баланс EUR - {int(exchange_eur)},'
+                                f' баланс USD - {int(exchange_usd)}')
                             break
             else:
                 print('Ошибка! Вы выбрали неправильное число!')
