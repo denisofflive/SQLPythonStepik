44307e6066f88297e2ead220ce19d83eaf906dcd
denis <denisofflive@mail.ru>
2024-01-24 20:23:25 +0300

    add Тестовое задание банкомат

--- 7.9 Тестовое задание по Банкомату №1/atm_7_9.py
+++ 7.9 Тестовое задание по Банкомату №1/atm_7_9.py
@@ -1,10 +1,11 @@
-from sql_query import SQL_atm
+from sql_query_7_9 import SQL_atm
 
 class ATM():
     def atm_logic(self):
 
         SQL_atm.create_table()
         SQL_atm.insert_users((1234, 1111, 10000))
+        SQL_atm.insert_users((2345, 2222, 10000))
         number_card = input("Введите пожалуйста номер карты: ")
 
         while True:
